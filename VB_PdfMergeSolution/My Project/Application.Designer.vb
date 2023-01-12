bed1eb8bf842b848f959d0151cd344bee141a326
tseop <77163842+tseop@users.noreply.github.com>
2023-01-12 18:08:20 +0900

    build: Update App Name

--- VB_PdfMergeSolution/My Project/Application.Designer.vb
+++ VB_PdfMergeSolution/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VB_PdfMergeSolution.PdfMerge
+            Me.MainForm = Global.PDF병합프로그램.PdfMerge
         End Sub
     End Class
 End Namespace
