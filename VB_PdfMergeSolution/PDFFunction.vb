36d61ea040d23c858ef24ed0df1952e310f94bed
tseop <77163842+tseop@users.noreply.github.com>
2023-01-12 14:00:26 +0900

    refactor: Separation class

---
+++ VB_PdfMergeSolution/PDFFunction.vb
@@ -0,0 +1,9 @@
+﻿Public Class PDFFunction
+    Public Sub Btn_Enabled()
+        PdfMerge.FileCleanBtn.Enabled = False
+        PdfMerge.FileDeleteBtn.Enabled = False
+        PdfMerge.FileMergeBtn.Enabled = False
+        PdfMerge.FileIndexUp.Enabled = False
+        PdfMerge.FileIndexDown.Enabled = False
+    End Sub
+End Class
---
+++ VB_PdfMergeSolution/PdfMerge.vb
@@ -0,0 +1,10 @@
+﻿Public Class PdfMerge
+    Public Shared main As PdfMerge
+    Private PDFFunction As PDFFunction
+    Private Sub PdfMerge_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PDFFunction.Btn_Enabled()
+    End Sub
+    Private Sub SelectFileBtn_Click(sender As Object, e As EventArgs) Handles SelectFileBtn.Click
+
+    End Sub
+End Class
